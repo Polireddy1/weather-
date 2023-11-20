4eb8304114fdd70751343747c0f0fdbf2db505f2
venky <venkateswarlu.polireddy@relanto.ai>
2023-11-20 12:32:11 +0530

    Commit V1

---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms'
+import { faSearch, faTemperatureHigh, faTemperatureLow, faWind, faDroplet, faClock } from '@fortawesome/free-solid-svg-icons';
+import { faSun, faMoon, } from '@fortawesome/free-solid-svg-icons';
+import { WeatherService } from '../service/weather.service';
+import { DatePipe } from '@angular/common';
+
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss']
+})
+export class LayoutComponent implements OnInit {
+
+  iconArray: any[] = [faSearch, faTemperatureHigh, faTemperatureLow, faDroplet, faWind, faClock];
+  weatherIcons: any[] = [faSun, faMoon];
+  weatherData: any;
+
+  submit = false;
+  localTime = '';
+  time: number = 10;
+  temperature = '0';
+  city = '';
+  country = '';
+  minTemp = "";
+  maxTemp = "";
+  wind = "";
+  humidity = "";
+
+
+  searchForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder, private weatherService: WeatherService,) { }
+
+  ngOnInit() {
+    this.searchForm = this.formBuilder.group({
+      city: [''] // Initial value of the city input field
+    });
+
+  }
+
+
+
+  onSubmit() {
+    if (this.searchForm.value.city == '') {
+      this.submit = false;
+    } else {
+      const city = this.searchForm.value.city;
+      this.submit = true;
+      this.weatherService.getWeatherData(city).subscribe(
+        (data: any) => {
+          this.weatherData = data;
+          this.updateWeatherInfo();
+        },
+        (error: any) => {
+          console.error('Failed to fetch weather data:', error);
+        }
+      );
+    }
+  }
+
+  updateWeatherInfo() {
+    if (this.weatherData && this.weatherData.location && this.weatherData.current) {
+      this.localTime = this.weatherData.location.localtime;
+      this.country = this.weatherData.location.country;
+      this.temperature = this.weatherData.current.temp_c;
+      this.city = this.weatherData.location.name;
+      this.minTemp = this.weatherData.current.mintemp_c;
+      this.maxTemp = this.weatherData.current.feelslike_c;
+      this.wind = this.weatherData.current.wind_kph;
+      this.humidity = this.weatherData.current.humidity;
+      this.time = new Date(this.weatherData.location.localtime).getHours();
+    }
+    console.log(this.time);
+  }
+}
